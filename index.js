66fba8e96a1336cb66dbfc526622f5d1ba7fe430
Hezron Sande <hezronsande2@gmail.com>
2026-03-18 13:23:14 +0300

    features added include tax caculation,finding amximum, calculation of dicount and palindrome if

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
+function calculateTax(amount){
+    return amount * 0.10;
+}
 
+function convertToUpperCase(text){
+    return text.toUpperCase();
+}
 
+function findMaximum(num1, num2){
+    if (num1 > num2){
+        return num1;
+    } else {
+        return num2;
+    }
+}
 
+function isPalindrome(word) {
+  const reversed = word.split('').reverse().join('');
+  return word === reversed;
+}
 
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+  const discount = (discountPercentage / 100) * originalPrice;
+  return originalPrice - discount;
+};
 
 // This is required for the test to function properly  
 module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
